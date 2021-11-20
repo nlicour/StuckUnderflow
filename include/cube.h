c6432ab565bba944aee3de4fc326fe50b6c27b0e
COLINE <coline.smagghe@gozmail.net>
2021-11-20 15:20:56 +0100

    Remplissage de la struct cube + fonctions light

--- include/cube.h
+++ include/cube.h
@@ -12,11 +12,18 @@ struct Color
 struct Vec3
 {
     uint8_t x, y, z;
+
+    bool operator==(const Vec3& other)
+    {
+        return  x == other.x &&
+                y == other.y &&
+                z == other.z;
+    }
 };
 
 namespace cube
 {
     void ligthTal(Cube* cube, Vec3 tal, Color color);
 
-    
+    void ligthLed(Cube* cube, Vec3 tal, unsigned int led, Color color);
 }
--- src/cube.cpp
+++ src/cube.cpp
@@ -1,6 +1,39 @@
 #include "cube.h"
+#include <vector>
 
-class Cube
+using namespace std;
+
+struct Cube
 {
-
+    std::vector<Tal> tals; 
 };
+
+struct Tal
+{
+    Vec3 pos;
+    Color leds[3];
+};
+
+namespace cube
+{
+    void ligthTal(Cube* cube, Vec3 tal, Color color)
+    {
+        for (unsigned int i = 0; i < 3; i++)
+        {
+            cube::ligthLed(cube, tal, i, color);
+        }
+    }
+
+    void ligthLed(Cube* cube, Vec3 talPos, unsigned int led, Color color)
+    {
+        for (Tal tmpTal : cube->tals)
+        {
+            if (tmpTal.pos == talPos)
+            {
+                tmpTal.leds[led].r = color.r;
+                tmpTal.leds[led].g = color.g;
+                tmpTal.leds[led].b = color.b;
+            }
+        }
+    }
+}

6e0459678f33a3b179569771253e522c35a8bdca
Quentin Debroise <quentin.debroise+git@gozmail.net>
2021-11-20 16:36:47 +0100

    -

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,96 @@ int main(int argc, char* argv[])
     static const uint16_t UNIVERSE_1 = 1;
     static const uint16_t UNIVERSE_2 = 2;
 
-    int sockfd = e131_socket();
-    if (sockfd < 0)
-    {
-        fprintf(stderr, "Couldn't create e131 socket.\n");
-        return -1;
-    }
+    int sockfd_u1 = e131_socket();
+    int sockfd_u2 = e131_socket();
 
-    e131_addr_t dest_universe_1;
-    if (e131_multicast_dest(&dest_universe_1, UNIVERSE_1, E131_DEFAULT_PORT) < 0)
-    {
-        fprintf(stderr, "Couldn't create a multicast destination on universe %d port %d\n", UNIVERSE_1, E131_DEFAULT_PORT);
-        return -1;
-    }
+    e131_addr_t dest_u1;
+    e131_addr_t dest_u2;
 
-    if (e131_multicast_join(sockfd, UNIVERSE_1) < 0)
-    {
-        fprintf(stderr, "Couldn't join socket to universe %d\n", UNIVERSE_1);
-        return -1;
-    }
+    e131_multicast_dest(&dest_u1, 1, E131_DEFAULT_PORT);
+    e131_multicast_dest(&dest_u2, 2, E131_DEFAULT_PORT);
+
+    e131_multicast_join(sockfd_u1, 1);
+    e131_multicast_join(sockfd_u2, 2);
 
     // Configure packet.
-    e131_packet_t packet;
-    e131_pkt_init(&packet, UNIVERSE_1, 510);
-    memcpy(packet.frame.source_name, "Stuckunderflower", 16);
+    e131_packet_t packet_u1;
+    e131_pkt_init(&packet_u1, 1, 510);
+    memcpy(packet_u1.frame.source_name, "Stuckunderflower", 16);
+    e131_set_option(&packet_u1, E131_OPT_PREVIEW, true);
+    packet_u1.frame.seq_number = 0;
 
-    if (e131_set_option(&packet, E131_OPT_PREVIEW, true) < 0)
+    e131_packet_t packet_u2;
+    e131_pkt_init(&packet_u2, 1, 66);
+    memcpy(packet_u2.frame.source_name, "Stuckunderflower", 16);
+    e131_set_option(&packet_u2, E131_OPT_PREVIEW, true);
+    packet_u2.frame.seq_number = 1;
+
+    // Clear universes.
+    e131_send(sockfd_u1, &packet_u1, &dest_u1);
+    e131_send(sockfd_u2, &packet_u2, &dest_u2);
+
+    packet_u1.frame.seq_number = 2;
+
+    /*
+    // 1 - 36 axe vertical.
+    //      1 - 9 -> 1TAL
+    //      10 - 18 -> 2TAL
+    uint32_t slot_start = 1;
+    uint32_t slot_end = slot_start + 9;
+    while (slot_end <= 510)
     {
-        fprintf(stderr, "Couldn't set e131 option.\n");
-        return false;
+        for (uint32_t i = slot_start; i <= slot_end; i+=3)
+        {
+            packet_u1.dmp.prop_val[i + 0] = 255;
+            packet_u1.dmp.prop_val[i + 1] = 255;
+            packet_u1.dmp.prop_val[i + 2] = 255;
+        }
+
+        if (e131_send(sockfd_u1, &packet_u1, &dest_u1) < 0)
+        {
+            fprintf(stderr, "Couldn't send packet_u1.\n");
+            e131_pkt_dump(stderr, &packet_u1);
+            return -1;
+        }
+        packet_u1.frame.seq_number++;
+        usleep(500000);
+
+        slot_end += 9;
     }
+    */
+
+    return 0;
+
+    /*
+    0
+        0
+    0       0
+        0       0
+    0       0
+        0
+    0
+    */
+
+    /*
+    packet.dmp.prop_val[1] = 255;
+    packet.dmp.prop_val[2] = 255;
+    packet.dmp.prop_val[3] = 255;
+
+    packet.dmp.prop_val[7] = 255;
+    packet.dmp.prop_val[8] = 255;
+    packet.dmp.prop_val[9] = 255;
 
     packet.dmp.prop_val[10] = 255;
     packet.dmp.prop_val[11] = 255;
     packet.dmp.prop_val[12] = 255;
 
+    packet.dmp.prop_val[13] = 255;
+    packet.dmp.prop_val[14] = 255;
+    packet.dmp.prop_val[15] = 255;
+    */
+
+    /*
     if (e131_pkt_validate(&packet) != E131_ERR_NONE)
     {
         fprintf(stderr, "Packet isn't well-formed.\n");
@@ -54,7 +109,7 @@ int main(int argc, char* argv[])
 
     for(;;)
     {
-        if (e131_send(sockfd, &packet, &dest_universe_1) < 0)
+        if (e131_send(sockfd_u1, &packet, &dest_u1) < 0)
         {
             fprintf(stderr, "Couldn't send packet.\n");
             e131_pkt_dump(stderr, &packet);
@@ -63,6 +118,7 @@ int main(int argc, char* argv[])
         packet.frame.seq_number++;
         usleep(250000);
     }
+    */
 
     return 0;
 }

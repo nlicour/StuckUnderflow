ebdf2bf440131061feccc2b48dfef54a34e75119
Quentin Debroise <quentin.debroise+git@gozmail.net>
2021-11-21 06:20:54 +0100

    Light remote leds upon a player's turn

--- src/game.cpp
+++ src/game.cpp
@@ -168,6 +168,8 @@ namespace game
 
     void play_turn(GameState &gs, RemoteSystem *rs, Cube *cube)
     {
+        remote::toggle_led(rs, gs.currentPlayer->id, 7);
+
         Vec3 move = {0, 0, 0};
         bool hasValidatedMove = false;
         while (!hasValidatedMove)
@@ -191,6 +193,8 @@ namespace game
                 }
             }
         }
+
+        remote::toggle_led(rs, gs.currentPlayer->id, 0);
     }
 
     void drawPlayer(Player &player, Cube *cube)
